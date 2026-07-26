cd247c4fa621f7565790bc277b4f801fb5f32ea7
fullmodel-star <fullmodel@gmail.com>
2026-07-26 12:13:01 +0000

    style(brand): 套用v4.3官方家族icon+頁首頁尾山形素材+sw版本+1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='cuoti-v18';
+const CACHE='cuoti-v19';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./ridgeline-ui.css','./favicon.svg','./icon.svg','./icon-180.png','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   // 不自動 skipWaiting：讓新版進入「等待」，由 App 詢問使用者後再接管
